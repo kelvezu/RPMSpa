01f1a2ade89c19a3bddad7594f2d83b7def2b162
cherrybombs13 <53111778+cherrybombs13@users.noreply.github.com>
2019-09-26 22:46:10 +0800

    disable security

--- includes/security.php
+++ includes/security.php
@@ -5,11 +5,12 @@ include "conn.inc.php";
 
 
 //REDIRECT THE USER IF NOT LOGGED IN
-if(empty($_SESSION['user_id'])){
-        header('location:loginpage.php');
-        exit();
+// if(empty($_SESSION['user_id'])){
+//         // header('location:loginpage.php');
+//         echo 'uncomment the header in php'
+//         exit();
 
-      }else{
+//       }else{
          '<b>user id:</b> '.$_SESSION['user_id'];
          ' <b>name:</b> '.$_SESSION['uname'];
          ' <b>position:</b> '.$_SESSION['position'];
@@ -19,6 +20,6 @@ if(empty($_SESSION['user_id'])){
           
          
     
-   }
+  //  }
 
 
