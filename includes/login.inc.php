8d2b1e85317304bfecf477dfb2d4117379c600a2
kelvezu <chrisraycun08@gmail.com>
2019-10-02 15:26:35 +0800

    10-2-2019

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -53,7 +53,7 @@ if (isset($_POST['login-submit'])) {
                     $_SESSION['school_id'] = $row['school_id'];
                     $position = $_SESSION['position'];
 
-                    //CHECK IF THE SCHOOL YEAR IS SET
+                    //CHECK IF THE SCHOOL YEAR IS SET   
                     if (!empty($sy_desc)) :
                         echo $_SESSION['sy'] = $sy_desc;
                         echo $_SESSION['sy_id'] = $sy_id;
