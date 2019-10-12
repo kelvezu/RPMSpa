8e12977594db459dcb1cf81271e0347d3ed6cd13
kelvezu <chrisraycun08@gmail.com>
2019-10-13 03:59:37 +0800

    finishing the devplan.php

--- devplan.php
+++ devplan.php
@@ -36,6 +36,7 @@
                     <input type="hidden" name="position" value="<?php echo $_SESSION['position'] ?>" />
                     <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id'] ?>" />
                     <input type="hidden" name="rater" value="<?php echo $_SESSION['rater'] ?>" />
+
                     <div id="A">
                         <fieldset>
                             <legend><strong>A. Functional Competencies</strong></legend>
@@ -48,6 +49,7 @@
                                         foreach ($esatForm2_LvlCap_results as $LvlCap_result) :
                                             ?>
                                             <li><b class="tomato-color">Key Result Area: </b><u><?php echo $LvlCap_result['kra_name'] ?></u> </li>
+
                                             <ul class="ul-square">
                                                 <?php
                                                         $queryMTobjlvlcap = 'SELECT kra_tbl.kra_name, mtobj_tbl.mtobj_name, esat2_objectivesmt_tbl.* FROM ( esat2_objectivesmt_tbl INNER JOIN kra_tbl ON esat2_objectivesmt_tbl.kra_id = kra_tbl.kra_id ) INNER JOIN mtobj_tbl ON esat2_objectivesmt_tbl.mtobj_id = mtobj_tbl.mtobj_id WHERE kra_tbl.kra_id = "' . $LvlCap_result['kra_id'] . '"';
@@ -55,10 +57,11 @@
                                                         foreach ($mtobjLvlcapResults as $mtobjLvlcap) :
                                                             ?>
                                                     <li><b class="darkred-color">Objective: </b><i><?php echo $mtobjLvlcap['mtobj_name'] ?></i></li><br />
+                                                    <input type="hidden" name="lvlcapmtobj_id[]" value="<?php echo $mtobjLvlcap['mtobj_id'] ?>" />
+                                                    <input type="hidden" name="lvlcapkra_id[]" value="<?php echo  $mtobjLvlcap['kra_id'] ?>" />
                                                 <?php endforeach; ?>
                                             </ul><br>
-                                            <input type="hidden" name="lvlcapkra_id[]" value="<?php echo  $LvlCap_result['kra_id'] ?>" />
-                                            <input type="hidden" name="lvlcapmtobj_id[]" value="<?php echo $LvlCap_result['mtobj_id'] ?>" />
+
                                         <?php
                                             endforeach;
                                             ?>
@@ -79,6 +82,7 @@
                                     foreach ($esatForm2_priodev_results as $PrioDev_result) :
                                         ?>
                                         <li><b class="tomato-color">Key Result Area: </b><u><?php echo $PrioDev_result['kra_name'] ?></u> </li>
+
                                         <ul class="ul-square">
                                             <?php
                                                     $queryMTobjpriodev = 'SELECT kra_tbl.kra_name, mtobj_tbl.mtobj_name, esat2_objectivesmt_tbl.* FROM ( esat2_objectivesmt_tbl INNER JOIN kra_tbl ON esat2_objectivesmt_tbl.kra_id = kra_tbl.kra_id ) INNER JOIN mtobj_tbl ON esat2_objectivesmt_tbl.mtobj_id = mtobj_tbl.mtobj_id WHERE kra_tbl.kra_id = "' . $PrioDev_result['kra_id'] . '"';
@@ -86,10 +90,10 @@
                                                     foreach ($mtobjPrioDevResults as $mtobjPriodev) :
                                                         ?>
                                                 <li><b class="darkred-color">Objective: </b><i><?php echo $mtobjPriodev['mtobj_name']  ?></i></li><br />
+                                                <input type="hidden" name="priodevmtobj_id[]" value="<?php echo $mtobjPriodev['mtobj_id'] ?>">
+                                                <input type="hidden" name="priodevkra_id[]" value="<?php echo  $mtobjPriodev['kra_id'] ?>">
                                             <?php endforeach; ?>
                                         </ul><br>
-                                        <input type="hidden" name="priodevkra_id[]" value="<?php echo  $PrioDev_result['kra_id'] ?>">
-                                        <input type="hidden" name="priodevmtobj_id[]" value="<?php echo $PrioDev_result['mtobj_id'] ?>">
                                     <?php
                                         endforeach;
                                         ?>
@@ -117,7 +121,7 @@
                             </div>
                         </div>
                     </div>
-
+                    <!-- Timeline and Resources needed -->
                     <div>
                         <div class="form-control-label bg-black">
                             <label for="learn-objectives" class="form-control-label bg-black">Timelines and Resources needed</label>
@@ -134,10 +138,118 @@
                         </div>
                     </div>
 
+                    <!-- B. Compentencies -->
+                    <hr>
+                    <legend><strong>B. Core Behavioral Competencies</strong></legend>
+                    <div id="B">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="bg-black"><label for="b_strength" class="form-control-label bg-black">Strengths</label></div>
+                                <ul class="ul">
+                                    <?php
+                                    if (count($esatForm3_strength_results)) :
+                                        foreach ($esatForm3_strength_results as $cbc_strength) :
+                                            ?>
+                                            <li><b><?php echo $cbc_strength['cbc_name'] ?></b></li>
+                                            <ul class="ul-square">
+                                                <?php $queryIndicatorStrength = 'SELECT cbc_indicators_tbl.*,esat3_core_behavioral_tbl.* FROM esat3_core_behavioral_tbl INNER JOIN cbc_indicators_tbl ON esat3_core_behavioral_tbl.cbc_ind_id = cbc_indicators_tbl.cbc_ind_id WHERE esat3_core_behavioral_tbl.cbc_id =  "' . $cbc_strength['cbc_id'] . '"';
+                                                        $indicatorStrengthResults = fetchAll($dbcon, $queryIndicatorStrength);
+                                                        foreach ($indicatorStrengthResults as $indicatorStrength) :
+                                                            ?>
+                                                    <li><?php echo $indicatorStrength['indicator']  ?></li>
+                                                    <input type="hidden" name="strength_cbc_id[]" value="<?php echo $indicatorStrength['cbc_id'] ?>" />
+                                                    <input type="hidden" name="strength_cbc_ind_id[]" value="<?php echo $indicatorStrength['cbc_ind_id'] ?>" />
+                                                <?php endforeach; ?>
+                                            </ul><br>
+                                        <?php
+                                            endforeach;
+                                            ?>
+                                </ul>
+                            <?php
+                            else :
+                                echo '<p class="no-record">No record!</p>';
+                            endif;
+                            ?>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="bg-black"><label for="a_strength" class="form-control-label bg-black">Development Needs</label></div>
+                                <ul class="ul">
+                                    <?php
+                                    if (count($esatForm3_devneeds_results)) :
+                                        foreach ($esatForm3_devneeds_results as $cbc_devneeds) :
+                                            ?>
+                                            <input type="hidden" name="cbc_id[]" value="<?php echo  $cbc_devneeds['cbc_id'] ?>" />
+                                            <li><b><?php echo $cbc_devneeds['cbc_name'] ?></b></li>
+                                            <ul class="ul-square">
+                                                <?php
+                                                        $queryIndicatorDevneeds = 'SELECT cbc_indicators_tbl.*,esat3_core_behavioral_tbl.* FROM esat3_core_behavioral_tbl INNER JOIN cbc_indicators_tbl ON esat3_core_behavioral_tbl.cbc_ind_id = cbc_indicators_tbl.cbc_ind_id WHERE esat3_core_behavioral_tbl.cbc_id =  "' . $cbc_devneeds['cbc_id'] . '"';
+                                                        $IndicatorDevNeedsResults = fetchAll($dbcon, $queryIndicatorDevneeds);
+                                                        foreach ($IndicatorDevNeedsResults as $indicatorDevneeds) :
+                                                            ?>
+                                                    <li><?php echo $indicatorDevneeds['indicator'] ?></li>
+                                                    <input type="hidden" name="devneed_cbc_id[]" value="<?php echo $indicatorDevneeds['cbc_id'] ?>" />
+                                                    <input type="hidden" name="devneed_cbc_ind_id[]" value="<?php echo $indicatorDevneeds['cbc_ind_id'] ?>" />
+                                                <?php endforeach;
+                                                        ?>
+                                            </ul><br>
+                                        <?php
+                                            endforeach;
+                                            ?>
+                                </ul>
+                            <?php
+                            else :
+                                echo '<p class="no-record">No record!</p>';
+                            endif;
+                            ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-control-label bg-black">
+                        <label for="learn-objectives" class="form-control-label bg-black">Action Plan</label>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="learning_objectives">Learning Objectives:</label>
+                            <textarea name="b_learning_objectives" id="" cols="30" rows="10" placeholder="Enter the Learning Objectives" class="form-control textarea"></textarea>
+                        </div>
+
+                        <div class="col-md-6">
+                            <label for="intervention">Interventions:</label>
+                            <textarea name="b_intervention" id="" cols="30" rows="10" placeholder="Enter the Interventions" class="form-control textarea"></textarea>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="form-control-label bg-black">
+                            <label for="learn-objectives" class="form-control-label bg-black">Timelines and Resources needed</label>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label for="timelines">Timelines:</label>
+                                <textarea name="b_timeline" id="" cols="30" rows="10" placeholder="Enter Timelines." class="form-control textarea"></textarea>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="resources_needed">Resources needed:</label>
+                                <textarea name="b_resources_needed" id="" cols="30" rows="10" placeholder="Enter the Resources needed." class="form-control textarea"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="learn-objectives" class="form-control-label">Feedback: </label>
+                        <textarea name="feedback" id="" cols="30" rows="10" class="form-control textarea" placeholder=" ________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________"></textarea>
+                    </div>
+
+
+
+
+
+
+
+
+
                     <br>
                     <div align="center">
-                        <input type="submit" name="submit" class="btn btn-primary" value="Submit" />
-
+                        <input type="submit" name="submit" class="btn btn-primary" placeholder="Submit" />
                     </div>
                 </form>
             </div>
--- includes/processdevplan.php
+++ includes/processdevplan.php
@@ -2,37 +2,63 @@
 
 
 include_once '../libraries/db.library.php';
+include_once '../libraries/func.lib.php';
 include_once 'constants.inc.php';
+include_once 'conn.inc.php';
+
 if (isset($_POST['submit'])) :
-    $sy = $_POST['sy'];
     $user_id = $_POST['user_id'];
+    $rater = $_POST['rater'];
+    $sy = $_POST['sy'];
     $school_id = $_POST['school_id'];
     $position = $_POST['position'];
-    $rater = $_POST['rater'];
-    $lvlcapkra_id = $_POST['lvlcapkra_id'];
-    $lvlcapmtobj_id = $_POST['lvlcapmtobj_id'];
-    $priodevkra_id = $_POST['priodevkra_id'];
-    $priodevmtobj_id = $_POST['priodevmtobj_id'];
+
+    $a_lvlcapkra_id = $_POST['lvlcapkra_id'];
+    $a_lvlcapmtobj_id = $_POST['lvlcapmtobj_id'];
+    $a_priodevkra_id = $_POST['priodevkra_id'];
+    $a_priodevmtobj_id = $_POST['priodevmtobj_id'];
     $a_learning_objectives = $_POST['a_learning_objectives'];
-    $a_action_plan = $_POST['a_learning_objectives'];
     $a_intervention = $_POST['a_intervention'];
     $a_timeline = $_POST['a_timeline'];
     $a_resources_needed = $_POST['a_resources_needed'];
-    // $b_action_plan = $_POST['b_action_plan'];
-    // $b_intervention = $_POST['b_intervention'];
-    // $b_timeline = $_POST['b_timeline'];
-    // $b_resources_needed = $_POST['b_resources_needed'];
-    // $feedback = $_POST['feedback'];
 
-    $conn = new mysqli('localhost', 'root', '', 'rpms');
-    $stmt = $conn->prepare("INSERT INTO `devplan_a_tbl`(`user_id`, `rater_id`, `sy`, `school`, `position`, `a_strengths`, `a_devneeds`, `a_learn_objectives`, `a_intervention`, `a_timeline`, `a_resource`) VALUES (?,?,?,?,?,?,?,?,?,?,?");
-    $stmt->bind_param("iiiisssssss", $user_id, $rater, $sy, $school_id, $position, $lvlcapkra_id, $priodevkra_id, $a_learning_objectives, $a_intervention, $a_timeline, $a_resources_needed);
-    $stmt->execute();
-    echo "New records created successfully";
-    $stmt->close();
-    $conn0->close();
-    // header("location:../devplan.php");
-    echo var_dump($_POST['submit']);
+    $strength_cbc_id = $_POST['strength_cbc_id'];
+    $strength_cbc_ind_id = $_POST['strength_cbc_ind_id'];
+
+    $devneed_cbc_id = $_POST['devneed_cbc_id'];
+    $devneed_cbc_ind_id = $_POST['devneed_cbc_ind_id'];
+    $b_learning_objectives = $_POST['b_learning_objectives'];
+    $b_intervention = $_POST['b_intervention'];
+    $b_timeline = $_POST['b_timeline'];
+    $b_resources_needed = $_POST['b_resources_needed'];
+
+
+    for ($count = 0; $count < count($a_lvlcapmtobj_id); $count++) {
+        $conn->query('INSERT INTO `devplanmt_a1_strength_tbl`( `user_id`, `rater_id`, `sy`, `school`, `position`, `a_strengths`, `strengths_mtobj`) VALUES ("' . $user_id . '","' . $rater . '","' . $sy . '","' . $school_id . '","' . $position . '","' . $a_lvlcapkra_id[$count] . '","' . $a_lvlcapmtobj_id[$count] . '" )') or die($conn->error);
+    }
+
+    for ($count = 0; $count < count($a_priodevmtobj_id); $count++) {
+        $conn->query('INSERT INTO `devplanmt_a2_devneeds_tbl`( `user_id`, `rater_id`, `sy`, `school`, `position`, `a_devneeds`, `devneeds_mtobj`) VALUES ("' . $user_id . '","' . $rater . '","' . $sy . '","' . $school_id . '","' . $position . '","' . $a_priodevkra_id[$count] . '","' . $a_priodevmtobj_id[$count] . '" )') or die($conn->error);
+    }
+
+    $conn->query('INSERT INTO `devplanmt_a3_actionplan_tbl`(`user_id`, `rater_id`, `sy`, `school`, `position`, `a_learning_objectives`, `a_intervention`, `a_timeline`, `a_resources_needed`) VALUES ("' . $user_id . '","' . $rater . '","' . $sy . '","' . $school_id . '","' . $position . '","' . $a_learning_objectives . '","' . $a_intervention . '","' . $a_timeline . '","' . $a_resources_needed . '")') or die($conn->error);
+
+
+    for ($count = 0; $count < count($strength_cbc_ind_id); $count++) {
+        $conn->query('INSERT INTO `devplanmt_b1_strength_tbl`( `user_id`, `rater_id`, `sy`, `school`, `position`, `strength_cbc_id`, `strength_cbc_ind_id`) VALUES ("' . $user_id . '","' . $rater . '","' . $sy . '","' . $school_id . '","' . $position . '","' . $strength_cbc_id[$count] . '","' . $strength_cbc_ind_id[$count] . '" )') or die($conn->error);
+    }
+
+    for ($count = 0; $count < count($devneed_cbc_ind_id); $count++) {
+        $conn->query('INSERT INTO `devplanmt_b2_devneeds_tbl`( `user_id`, `rater_id`, `sy`, `school`, `position`, `devneed_cbc_id`, `devneed_cbc_ind_id`) VALUES ("' . $user_id . '","' . $rater . '","' . $sy . '","' . $school_id . '","' . $position . '","' . $devneed_cbc_id[$count] . '","' . $devneed_cbc_ind_id[$count] . '" )') or die($conn->error);
+    }
+
+    $conn->query('INSERT INTO `devplanmt_b3_actionplan_tbl`(`user_id`, `rater_id`, `sy`, `school`, `position`, `b_learning_objectives`, `b_intervention`, `b_timeline`, `b_resources_needed`) VALUES ("' . $user_id . '","' . $rater . '","' . $sy . '","' . $school_id . '","' . $position . '","' . $b_learning_objectives . '","' . $b_intervention . '","' . $b_timeline . '","' . $b_resources_needed . '")') or die($conn->error);
+
+
+    // echo count($devplanmt_b3_actionplan_tbl);
+    echo 'complete!';
+    pre_r($_POST['submit']);
+
 
 
 
