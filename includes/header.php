de2339be48fe9378d59b6d345771755f1744057a
cherrybombs13 <guerramadz134@gmail.com>
2019-10-26 10:33:46 +0800

    cot link

--- includes/header.php
+++ includes/header.php
@@ -125,8 +125,8 @@ $position = $_SESSION['position'];
                         </li>
                         <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-star"></i> <span class="mini-dn">COT</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
                             <div role="menu" class="dropdown-menu pre-scrollable left-menu-dropdown chart-left-menu-std animated flipInX">
-                                <a href="#" class="dropdown-item">View Teacher COT</a>
-                                <a href="#" class="dropdown-item">View Master Teacher COT</a>
+                                <a href="viewcotrateT.php" class="dropdown-item">View Teacher COT</a>
+                                <a href="viewcotrateMT.php" class="dropdown-item">View Master Teacher COT</a>
                             </div>
                         </li>
                         <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-table"></i> <span class="mini-dn">IPCRF</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
