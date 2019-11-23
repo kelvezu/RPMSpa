db1c1c1a2407ba75f49e5aab63494647a15d49f9
cherrybombs13 <53111778+cherrybombs13@users.noreply.github.com>
2019-11-23 15:09:40 +0800

    added attach MT MOV

---
+++ attachfileMT.php
@@ -0,0 +1,148 @@
+
+<?php
+
+include 'sampleheader.php';
+?>
+
+
+
+<h2 class="text-center">Means of Verification</h2>
+<br>
+
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-6">
+    <div class="card"  style="height:500px;">
+        <div class="card-header text-center">
+            List of MOV's
+        </div>
+
+        <div class="card-body box">
+        
+            
+            <table class="table table-hover table-responsive-sm table-sm ">
+                    <!-- Start loop for  KRA -->
+                    <?php
+                    //FETCH THE FIELDS FROM THE DB  
+                    $kra_num = 0;
+                    $tobj_num = 1;
+                    $result = $conn->query('SELECT * FROM kra_tbl')  or die($conn->error);
+                    while ($row = $result->fetch_assoc()) :
+                        $kra_id = $row['kra_id'];
+                        $kra_name = $row['kra_name'];
+                        $kra_num++;
+
+                        ?>
+                        <thead class="thead-dark">
+                            <tr>
+                                <!-- ASSIGN THE VALUE FROM THE DB  -->
+                                <th class="bg-info"><small><?php echo "KRA " . $kra_num; ?></small></th>
+                                <th class="bg-info"><small>Main MOV</small></th>
+                                <th class="bg-info"><small>Supporting MOV</small></th>
+                            </tr>
+                        </thead>
+
+
+                        <tbody>
+                            <tr>
+                                <!-- START OF LOOP FROM OBJECTIVE -->
+                                <?php
+                                    //QUERY FOR INDICATORS TABLE 
+                                    $indresult = $conn->query("SELECT * FROM mtobj_tbl WHERE kra_id = '$kra_id'")  or die($conn->error);
+                                    //FETCH THE DATA FROM INDICATOR TABLE
+
+                                    while ($rows = $indresult->fetch_assoc()) :
+
+                                        ?>
+                                    <td><small>
+
+                                        <?php
+                                                //ASSIGN THE VALUE FROM THE DB
+
+                                                echo 'Objective' . $mtobj_id = $rows['mtobj_id'];
+
+                                                ?>
+                                                </small>
+                                    </td>
+
+                                <?php
+                                    //QUERY FOR MOV TABLE 
+                                    $movresult = $conn->query("SELECT * FROM mtmov_tbl WHERE kra_id = '$kra_id' AND mtobj_id = '$mtobj_id'")  or die($conn->error);
+                                    while($rowss = $movresult->fetch_assoc()):
+                                ?>
+                                    <td><small><?php echo $rowss['main_mov']; ?></small></td>
+                                    <td><small><?php echo $rowss['supp_mov']; ?></small></td>
+                            </tr>
+                        </tbody>
+                        <?php endwhile; ?>
+                        <?php endwhile; ?>
+                        <?php endwhile; ?>
+            </table>
+        
+
+        </div>
+    </div>
+    </div>
+<div class="col-sm-6">
+    <div class="card" style="height:500px;">
+        <div class="card-header text-center">
+            Upload
+        </div>
+        <div class="card-body box"> 
+            <div class="row">
+                <div class="col">
+                    <form action="includes/uploadMT.php" method="post" enctype="multipart/form-data">
+                        <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id'];?>">
+                        <input type="hidden" name="position" value="<?php echo $_SESSION['position'];?>">
+                        <input type="hidden" name="rater_id" value="<?php echo $_SESSION['rater'];?>">
+                        <input type="hidden" name="school_id" value="<?php echo $_SESSION['school_id'];?>">
+                        <input type="hidden" name="sy_id" value="<?php echo $_SESSION['active_sy_id'];?>">
+
+
+                        <input type="file" name="file" class=" btn btn-outline-info form-control" required><br>
+                        <label for="desc"><strong>Choose MOV Type:</strong></label><br>
+                            <select name="mov_type" class="form-control" required>
+                                <option value="" disabled selected>--Select MOV Type--</option>
+                                <option name="main_mov" value="main_mov">Main MOV</option>
+                                <option name="supp_mov" value="supp_mov">Supporting MOV</option>
+                            </select><br>
+                        <label for="desc"><strong>Description</strong></label><br>
+                        <textarea name="description" cols="40" rows="10" required></textarea><br>
+                       
+                </div>
+               
+                <div class="col">
+                    <label for="selectkra"><strong>Choose Objective</strong></label><br>
+
+                    <?php
+                        $obj_id = 0;
+                        $qry = $conn->query("SELECT * FROM mtobj_tbl");
+                        while ($resultQry = $qry->fetch_assoc()) :
+                            $kra_id = $resultQry['kra_id'];
+                            $obj_id = $resultQry['mtobj_id'];
+
+                    ?>
+                         <input type="checkbox" name="obj[]" value="<?php echo $obj_id ;?>" /><?php echo "<a data-toggle='tooltip' data-placement='top' title='".displayKRA($conn,$kra_id)."'>KRA ". $kra_id . "</a> - <a data-toggle='tooltip' data-placement='top' title='".displayObjectiveMT($conn,$obj_id)."'>Objective " . $obj_id. "</a>"; ?><br>
+                       
+                        <?php endwhile; ?>
+                        <button type="submit" name="submit" class="btn btn-info">Upload</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>        
+</div>
+
+
+</div>
+
+
+       
+</form>
+
+<?php
+
+include 'samplefooter.php';
+?>
--- includes/uploadMT.php
+++ includes/uploadMT.php
@@ -32,24 +32,24 @@ if(isset($_POST['submit'])):
         if($fileError === 0):
             if($fileSize < 100000):
                 $filenameNew = uniqid('', true) . "." . $fileActualExt;
-                    $fileDestination = '../attachments/Teacher' . $filenameNew;
+                    $fileDestination = '../attachments/Master Teacher/' . $filenameNew;
                     move_uploaded_file($fileTmpName, $fileDestination);
 
-                    $filepath = "localhost/rpmspa/attachments/Teacher" . $filenameNew;
+                    $filepath = "localhost/rpmspa/attachments/Master Teacher/" . $filenameNew;
 
-                    $mov_qry = "INSERT INTO `mov_a_t_attach_tbl` (`attachment`,`file_type`, `mov_type`, `user_id`, `position`, `rater_id`, `school_id`, `sy_id`) VALUES ('".$filepath."','".$fileActualExt."','".$mov_type."',$user_id,'".$position."',$rater_id,$school_id,$sy_id)";
+                    $mov_qry = "INSERT INTO `mov_a_mt_attach_tbl` (`attachment`,`file_type`, `mov_type`, `user_id`, `position`, `rater_id`, `school_id`, `sy_id`) VALUES ('".$filepath."','".$fileActualExt."','".$mov_type."',$user_id,'".$position."','.$rater_id.','.$school_id.','.$sy_id.')";
                     $mov_results = mysqli_query($conn,$mov_qry) OR die($conn->error);
 
                     if($mov_results){
                         $last_id = mysqli_insert_id($conn);
 
                         for($count = 0; $count < count($obj_id); $count++){
-                            $qry = "INSERT INTO `mov_b_t_attach_tbl`(`mov_id`, `file_type`, `mov_type`, `kra_id`, `obj_id`, `user_id`, `position`, `rater_id`, `school_id`, `sy_id`) VALUES ('$last_id','".$fileActualExt."','$mov_type',".displayKRAidofTobj($conn,$obj_id[$count]).",'$obj_id[$count]','$user_id','$position','$rater_id','$school_id','$sy_id')";
+                            $qry = "INSERT INTO `mov_b_mt_attach_tbl`(`mov_id`, `file_type`, `mov_type`, `kra_id`, `obj_id`, `user_id`, `position`, `rater_id`, `school_id`, `sy_id`) VALUES ('.$last_id.','".$fileActualExt."','.$mov_type.',".displayKRAidofTobj($conn,$obj_id[$count]).",'.$obj_id[$count].','.$user_id.','.$position.','.$rater_id.','.$school_id.','.$sy_id.')";
                             $mov_attach_qry = mysqli_query($conn,$qry) OR die($conn->error);
                         }
                     }
                    
-                header("Location:../attachfile.php?=success");
+                header("Location:../attachfileMT.php?=success");
             else:
                 echo "Your file is too big!";
             endif;
