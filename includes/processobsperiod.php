94222bd22f6b4335bde66607fdd1d7902de0df41
kelvezu <chrisraycun08@gmail.com>
2019-12-08 15:15:02 +0800

    update

--- includes/processobsperiod.php
+++ includes/processobsperiod.php
@@ -17,6 +17,8 @@ if (isset($_POST['submit_obs'])) :
     $third_period = $_POST['third_period'];
     $fourth_period = $_POST['fourth_period'];
 
+    $qry1 = $conn->query('UPDATE obs_period_tbl SET `status` = "Inactive" WHERE school = ' . $school . ' ');
+
     $qry = 'INSERT INTO `obs_period_tbl`( `first_period`, `second_period`, `third_period`, `fourth_period`, `sy`, `school`, `status`,  `created_by`) VALUES ("' . $first_period . '","' . $second_period . '","' . $third_period . '","' . $fourth_period . '","' . $sy . '","' . $school . '","' . $status . '","' . $created_by . '")';
     $result = mysqli_query($conn, $qry) or die(mysqli_error($conn));
 
